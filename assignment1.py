5cb3bfab818b06bea41dc251ea1ebf9bbaf5f75e
Kshama Pandit <kshamapandit@Kshamas-MacBook-Air.local>
2020-07-09 09:18:59 -0700

    message

---
+++ assignment1.py
@@ -0,0 +1,10 @@
+states = {'kentucky':4468000, 'virginia':8536000, 'north carolina':10049000, 
+'georgia':10062000, 'alabama':4903000, 'mississippi':2976000, 'arkansas':3018000, 
+'missouri':6137000}
+states['alabama'] = 4903000
+print(states)
+
+
+print(states['virginia'])
+
+print(sorted(states))
